b5e2c49d833e0b7da2359fe6a47ebbdc63db6941
SadHappy <craigjm19@gmail.com>
2024-03-19 10:35:26 +0000

    Added Isogram C# solution

---
+++ C#/Isogram/Isogram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        string letters = new String(word.ToLower().Where(Char.IsLetter).ToArray());
+
+        return letters.ToHashSet().Count() == letters.Length;
+    }
+}
---
+++ C#/Isogram/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] words = ["lumberjacks",
+    "background",
+    "downstream",
+    "six-year-old",
+    "alphAbet",
+    "Alphabet",
+    ];
+
+foreach ( var word in words )
+{
+    Console.WriteLine($"{word}: {Isogram.IsIsogram(word)}");
+}
