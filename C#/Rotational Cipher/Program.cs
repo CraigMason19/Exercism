3949658f39fb0cd03186346560d4e0896ac8899f
SadHappy <craigjm19@gmail.com>
2024-03-19 10:13:56 +0000

    Added Rotational Cipher C# solution

---
+++ C#/Rotational Cipher/Program.cs
@@ -0,0 +1,9 @@
+﻿var phrase = "The quick brown fox jumps over the lazy dog.";
+var phrase_rot13 = "Gur dhvpx oebja sbk whzcf bire gur ynml qbt.";
+
+var encodedPhrase = RotationalCipher.Rotate(phrase, 13);
+
+Console.WriteLine($"Orginal: {phrase}");
+Console.WriteLine($"Answer: {phrase_rot13}");
+Console.WriteLine($"MyCode: {encodedPhrase}");
+Console.WriteLine(encodedPhrase == phrase_rot13);
---
+++ C#/Rotational Cipher/RotationalCipher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+public static class RotationalCipher
+{
+    public static readonly string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static char RotateLetter(char c, int shiftKey)
+    {
+        if (shiftKey == 0 || shiftKey == 26 || !char.IsLetter(c))
+        {
+            return c;
+        }
+
+        int index = (letters.IndexOf(char.ToLower(c)) + shiftKey) % 26;
+     
+        return char.IsLower(c) ? letters[index] : letters[index+26];
+    }
+
+    public static string Rotate(string text, int shiftKey)
+    {
+        if(shiftKey == 0 || shiftKey == 26)
+        {
+            return text;
+        }
+
+        StringBuilder sb = new StringBuilder("");
+        foreach(char c in text)
+        {
+            sb.Append(RotateLetter(c, shiftKey));
+        }
+
+        return sb.ToString();
+    }
+}
