82b9ee1e6fb260641f94cb4b2ebfa9ac077434c0
SadHappy <craigjm19@gmail.com>
2024-03-20 16:23:56 +0000

    Added Run-Length Encoding C# Solution

---
+++ C#/Run-Length Encoding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+string[] plainStrings = [
+    "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB",
+    "  hsqq qww  ",
+    "AABCCCDEEEE",
+    ];
+
+foreach( string s in plainStrings)
+{
+    Console.WriteLine($"\nOriginal:{s}");
+
+    var encoded = RunLengthEncoding.Encode(s);
+    var decoded = RunLengthEncoding.Decode(encoded);
+
+    Console.WriteLine($"encoded:{encoded}");
+    Console.WriteLine($"decoded:{decoded}");
+}
---
+++ C#/Run-Length Encoding/RunLengthEncoding.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class RunLengthEncoding
+{
+    public static string Encode(string input)
+    {
+        var pattern = @"(.)\1*";
+        Regex r = new Regex(pattern);
+
+        string output = "";
+
+        foreach(Match match in r.Matches(input))
+        {
+            if (match.Value.Length == 1)
+            {
+                output += $"{match.Value.First()}";
+            }
+            else
+            {
+                output += $"{match.Value.Length}{match.Value.First()}";
+            }
+        }
+
+        return output;
+    }
+
+    public static string Decode(string input)
+    {
+        var pattern = @"(\d+.|.)\1*";
+        Regex r = new Regex(pattern);      
+
+        string output = "";
+
+        foreach (Match match in r.Matches(input))
+        {
+            if(match.Value.Length == 1) 
+            {
+                output += match.Value.ToString();
+            }
+            else
+            {
+                // The end of the string is a letter and everything else is a number
+                // 22B for example
+                int count = Int32.Parse(match.Value.Substring(0, match.Value.Length-1));
+                output += new string(match.Value.Last(), count);
+            }
+        }
+
+        return output;
+    }
+}
