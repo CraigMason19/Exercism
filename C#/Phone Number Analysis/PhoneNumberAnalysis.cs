e5995d7809ac0ac6e53e1679dcf77286e7929da7
SadHappy <craigjm19@gmail.com>
2024-03-26 17:13:00 +0000

    Added C# Phone Number Analysis solution

---
+++ C#/Phone Number Analysis/PhoneNumberAnalysis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class PhoneNumber
+{
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+    {
+        bool a = phoneNumber.Substring(0, 3) == "212";
+        bool b = phoneNumber.Substring(4, 3) == "555";
+        string c = phoneNumber.Substring(8, 4);
+
+        return (a, b, c);
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+    {
+        return phoneNumberInfo.IsFake;
+    }
+}
---
+++ C#/Phone Number Analysis/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] numbers = ["212-555-1234",
+    "555-123-6789",
+];
+
+foreach(string number in numbers)
+{
+    Console.WriteLine($"{number} -> {PhoneNumber.Analyze(number)}");
+}
