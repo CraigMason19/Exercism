250dc3ed46a131e076b0659ab993bd2321b6cf33
SadHappy <craigjm19@gmail.com>
2024-04-08 09:33:15 +0100

    Added C# Matching Brackets solution

---
+++ C#/Matching Brackets/MatchingBrackets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class MatchingBrackets
+{
+    static readonly Dictionary<char, char> _brackets = new Dictionary<char, char>{
+            { '[', ']' },
+            { '{', '}' },
+            { '(', ')' },
+        };
+
+    static string OnlyBrackets(string s)
+    {
+        char[] b = _brackets.Keys.Union(_brackets.Values).ToArray();
+        return string.Join("", s.Where(c => b.Contains(c)));
+    }
+
+    public static bool IsPaired(string input)
+    {
+        if (string.IsNullOrEmpty(input)) { return true; }
+
+        Stack<char> bracketsStack = new Stack<char>();
+        string extraBrackets = "";
+
+        foreach(char c in OnlyBrackets(input))
+        {
+            if(_brackets.ContainsKey(c))
+            {
+                bracketsStack.Push(c);
+                continue;
+
+            }
+
+            if (bracketsStack.Count > 0)
+            {
+                if (c == _brackets[bracketsStack.Peek()])
+                {
+                    bracketsStack.Pop();
+                    continue;
+                }
+            }
+
+            extraBrackets += c;
+        }
+
+        return bracketsStack.Count == 0 && extraBrackets.Length == 0;
+    }
+}
---
+++ C#/Matching Brackets/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] inputs = [
+    "{[)][]}",
+    "}{",
+    "",
+    "(((185 + 223.85) * 15) - 543)/2",
+    "[ ]",
+    "{{",
+    "{{]}",
+    "[]{}()",
+];
+
+foreach (var input in inputs)
+{
+    Console.WriteLine($"{input} -> Brackets match -> {MatchingBrackets.IsPaired(input)}");
+}
