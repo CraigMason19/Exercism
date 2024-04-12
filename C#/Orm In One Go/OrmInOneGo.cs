b4b350372562ce4e3ef1c50e437d96501ebc963a
SadHappy <craigjm19@gmail.com>
2024-04-12 11:52:42 +0100

    Added 2 ORM solutions in C#

---
+++ C#/Orm In One Go/OrmInOneGo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Orm
+{
+    private Database database;
+
+    public Orm(Database database)
+    {
+        this.database = database;
+    }
+
+    public void Write(string data)
+    {
+        using (var database = this.database)
+        {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+        }
+    }
+
+    public bool WriteSafely(string data)
+    {
+        using (this.database) ;
+        try
+        {
+            this.database.BeginTransaction();
+            this.database.Write(data);
+            this.database.EndTransaction();
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
