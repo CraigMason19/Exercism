d100c39c7e27340e7f51769373cc7211190d6e99
SadHappy <craigjm19@gmail.com>
2024-04-15 10:27:33 +0100

    Added Booking Up For Beauty C# solution

---
+++ C#/Booking Up For Beauty/Program.cs
@@ -0,0 +1,7 @@
+﻿var dt = new DateTime(2019, 07, 25, 13, 45, 0);
+
+Console.WriteLine(Appointment.Schedule("7/25/2019 13:45:00"));
+Console.WriteLine(Appointment.HasPassed(dt));
+Console.WriteLine(Appointment.IsAfternoonAppointment(dt));
+Console.WriteLine(Appointment.Description(dt));
+Console.WriteLine(Appointment.AnniversaryDate());
