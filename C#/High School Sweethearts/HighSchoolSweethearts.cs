6dfb4107af01a1a941b8e212aa6ab03caecfa2bf
SadHappy <craigjm19@gmail.com>
2024-04-17 12:19:50 +0100

    Added High School Sweetherts C# solution

---
+++ C#/High School Sweethearts/HighSchoolSweethearts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+public static class HighSchoolSweethearts
+{
+    public static string DisplaySingleLine(string studentA, string studentB)
+    {
+        // 1 for the heart and 2 spaces each side of it
+        const int l = (61 - 3) / 2;
+
+        return $"{studentA,l} ♡ {studentB,-l}";
+    }
+
+    public static string DisplayBanner(string studentA, string studentB)
+    {
+        var initials = (studentA + studentB).Where(char.IsAsciiLetterUpper).ToArray();
+
+        var asciiHeart = String.Format(@"     ******       ******
+   **      **   **      **
+ **         ** **         **
+**            *            **
+**                         **
+**     {0}. {1}.  +  {2}. {3}.     **
+ **                       **
+   **                   **
+     **               **
+       **           **
+         **       **
+           **   **
+             ***
+              *", initials[0], initials[1], initials[2], initials[3]);
+
+        return asciiHeart;
+    }
+
+    public static string DisplayGermanExchangeStudents(string studentA
+        , string studentB, DateTime start, float hours)
+    {
+        var x = start.GetDateTimeFormats();
+        return $"{studentA} and {studentB} have been dating since {start.ToString("dd.MM.yyyy")} - that's {hours.ToString("N2", CultureInfo.GetCultureInfo("de-DE"))} hours";
+    }
+}
---
+++ C#/High School Sweethearts/Program.cs
@@ -0,0 +1,8 @@
+﻿string studentA = "Lance Green";
+string studentB = "Pat Riley";
+
+Console.WriteLine(HighSchoolSweethearts.DisplaySingleLine(studentA, studentB));
+
+Console.WriteLine(HighSchoolSweethearts.DisplayBanner(studentA, studentB));
+
+Console.WriteLine(HighSchoolSweethearts.DisplayGermanExchangeStudents("Norbert", "Heidi", new DateTime(2019, 1, 22), 1535.22f));
