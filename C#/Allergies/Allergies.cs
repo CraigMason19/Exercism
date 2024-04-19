4c07527e82368bc83686a2b2131cc53a6d53483f
SadHappy <craigjm19@gmail.com>
2024-04-19 12:44:15 +0100

    Added Allergies soultion in C#

---
+++ C#/Allergies/Allergies.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Allergen
+{
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+public class Allergies
+{
+    List<Allergen> _allergies = new List<Allergen>();
+
+    public Allergies(int mask)
+    {
+        _allergies = Enum.GetValues(typeof(Allergen))
+                         .Cast<Allergen>()
+                         .Where(x => ((int)x & mask) != 0)
+                         .ToList();
+
+        //foreach (var allegen in Enum.GetValues(typeof(Allergen)))
+        //{
+        //    var result = (mask & (int)allegen);
+
+        //    if (result != 0)
+        //    {
+        //        _allergies.Add((Allergen)allegen);
+        //    }
+        //}
+    }
+
+    public bool IsAllergicTo(Allergen allergen) => _allergies.Contains(allergen);
+
+    public Allergen[] List() => _allergies.ToArray();
+}
---
+++ C#/Allergies/Program.cs
@@ -0,0 +1,7 @@
+﻿var person = new Allergies(9);
+
+Console.WriteLine(person); 
+Console.WriteLine(string.Join(", ", person.List()));
+
+Console.WriteLine($"Is allergic to Eggs: {person.IsAllergicTo(Allergen.Eggs)}");
+Console.WriteLine($"Is allergic to ShellFish: {person.IsAllergicTo(Allergen.Shellfish)}");
