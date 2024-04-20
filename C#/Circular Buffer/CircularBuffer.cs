88adc78c68e4353d24a255eeb403dc2765898062
SadHappy <craigjm19@gmail.com>
2024-04-20 21:03:49 +0100

    Added Circular Buffer solution in C#

---
+++ C#/Circular Buffer/CircularBuffer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CircularBuffer<T>
+{
+    private List<T> _data;
+    private int _writeIndex = 0;
+    private int _readIndex = 0;
+
+    public int Capacity { get; private set; } = 0;
+
+    public int Count { get; private set; } = 0;
+
+    private static int AdvanceIndex(int index, int length) => (index + 1) % length;
+
+    public CircularBuffer(int capacity)
+    {
+        Capacity = capacity;
+        Clear();
+    }
+
+    public void Write(T value)
+    {
+        if (this.Count == Capacity)
+        {
+            throw new InvalidOperationException("Buffer full");
+        }
+
+        _data[_writeIndex] = value;
+        _writeIndex = AdvanceIndex(_writeIndex, Capacity);
+        Count++;
+    }
+
+    public T Read()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("Buffer Empty");
+        }
+
+        T value = this._data[_readIndex];
+
+        _data[_readIndex] = default; // Reset to default value (null for reference types)
+        _readIndex = AdvanceIndex(_readIndex, Capacity);
+
+        Count--;
+
+        return value;
+    }
+
+    public void Overwrite(T value)
+    {
+        if (Count != Capacity)
+        {
+            Write(value);
+        }
+        else
+        {
+            _data[_readIndex] = value;
+            _readIndex = AdvanceIndex(_readIndex, Capacity);
+        }
+    }
+
+    public void Clear()
+    {
+        _data = new List<T>(Capacity);
+
+        for (int i = 0; i < Capacity; i++)
+        {
+            _data.Add(default);
+        }
+
+        this.Count = 0;
+        this._writeIndex = 0;
+        this._readIndex = 0;
+    }
+}
---
+++ C#/Circular Buffer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+var buffer = new CircularBuffer<int>(3);
+buffer.Write(0);
+buffer.Write(4);
+buffer.Write(3);
+
+Console.WriteLine(buffer.Read()); // 1;
+
+buffer.Write(4);
+buffer.Overwrite(5);
+
+Console.WriteLine(buffer.Read()); // 3
+Console.WriteLine(buffer.Read()); // 4
+Console.WriteLine(buffer.Read()); // 5
+
+Console.WriteLine(buffer.Count);  // 0
