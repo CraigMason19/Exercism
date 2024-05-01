c2f6df3a7cf2d0c31a7fc25fca705071ce3df621
SadHappy <craigjm19@gmail.com>
2024-05-01 20:36:40 +0100

    Added C# Face ID 2.0 solution

---
+++ C#/Face ID 2.0/FaceID2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FacialFeatures
+{
+    public string EyeColor { get; }
+    public decimal PhiltrumWidth { get; }
+
+    public FacialFeatures(string eyeColor, decimal philtrumWidth)
+    {
+        EyeColor = eyeColor;
+        PhiltrumWidth = philtrumWidth;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is FacialFeatures other)
+        {
+            if (EyeColor == other.EyeColor && PhiltrumWidth == other.PhiltrumWidth)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return EyeColor.GetHashCode() ^ PhiltrumWidth.GetHashCode();
+    }
+}
+
+public class Identity
+{
+    public string Email { get; }
+    public FacialFeatures FacialFeatures { get; }
+
+    public Identity(string email, FacialFeatures facialFeatures)
+    {
+        Email = email;
+        FacialFeatures = facialFeatures;
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj is Identity other)
+        {
+            if (Email == other.Email && FacialFeatures.Equals(other.FacialFeatures))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return Email.GetHashCode() ^ FacialFeatures.GetHashCode();
+    }
+}
+
+public class Authenticator
+{
+    private HashSet<Identity> _identities = new HashSet<Identity>();
+
+    public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB)
+    {
+        return faceA.Equals(faceB);
+    }
+
+    public bool IsAdmin(Identity identity)
+    {
+        var admin = new Identity("admin@exerc.ism", new FacialFeatures("green", 0.9m));
+        return identity.Equals(admin);
+    }
+
+    public bool Register(Identity identity)
+    {
+        var result = _identities.Add(identity);
+        return result;
+    }
+
+    public bool IsRegistered(Identity identity)
+    {
+        return _identities.Contains(identity);
+    }
+
+    public static bool AreSameObject(Identity identityA, Identity identityB)
+    {
+        return object.ReferenceEquals(identityA, identityB);
+    }
+}
---
+++ C#/Face ID 2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿var faceA = new FacialFeatures("green", 0.9m);
+var faceB = new FacialFeatures("blue", 0.9m);
+
+Console.WriteLine(Authenticator.AreSameFace(faceA, new FacialFeatures("green", 0.9m)));
+Console.WriteLine(Authenticator.AreSameFace(faceA, faceB));
+
+var authenticator = new Authenticator();
+
+Console.WriteLine(authenticator.IsAdmin(new Identity("admin@exerc.ism", new FacialFeatures("green", 0.9m))));
+Console.WriteLine(authenticator.IsAdmin(new Identity("admin@thecompetition.com", new FacialFeatures("green", 0.9m))));
