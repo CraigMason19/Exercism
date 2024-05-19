53a2dcf44f0739d8ffef30e5e1eb4a859be86844
SadHappy <craigjm19@gmail.com>
2024-05-19 14:04:17 +0100

    Added Grains solution in C#

---
+++ C#/Grains/Grains.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        return n switch
+        {
+            < 1 or > 64 => throw new ArgumentOutOfRangeException("Invalid chess board square"),
+            1 => 1,
+            _ => (ulong)BigInteger.Pow(2, n - 1)
+        };
+    }
+
+    public static ulong Total()
+    {
+        return (ulong)(BigInteger.Pow(2, 64) - new BigInteger(1));
+    }
+}
---
+++ C#/Grains/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+for(int i = 1; i < 65; i++)
+{
+    Console.WriteLine($"{i} -> {Grains.Square(i)}");
+}
+
+Console.WriteLine($"Total -> {Grains.Total()}");
