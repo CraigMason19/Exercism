efc831b4087f37caae402d0033d32b3802e62852
SadHappy <craigjm19@gmail.com>
2024-05-21 14:14:13 +0100

    Added Hyperia Forex & Wizards & Warriors 2.0 solutions in C#

---
+++ C#/Hyperia Forex/HyperiaForex.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+public struct CurrencyAmount
+{
+    private decimal amount;
+    private string currency;
+
+    public CurrencyAmount(decimal amount, string currency)
+    {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    // Implement equality operators
+    public static bool operator !=(CurrencyAmount a, CurrencyAmount b)
+    {
+        if (a.currency != b.currency)
+        {
+            throw new ArgumentException();
+        }
+
+        return a.amount != b.amount;
+    }
+
+    public static bool operator ==(CurrencyAmount a, CurrencyAmount b)
+    {
+        if (a.currency != b.currency)
+        {
+            throw new ArgumentException();
+        }
+
+        return a.amount == b.amount;
+    }
+
+
+
+
+    // Implement comparison operators
+    public static bool operator >(CurrencyAmount a, CurrencyAmount b)
+    {
+        if (a.currency != b.currency)
+        {
+            throw new ArgumentException();
+        }
+
+        return a.amount > b.amount;
+    }
+
+    public static bool operator <(CurrencyAmount a, CurrencyAmount b)
+    {
+        if (a.currency != b.currency)
+        {
+            throw new ArgumentException();
+        }
+
+        return a.amount < b.amount;
+    }
+
+    // Arithmetic operators
+    public static CurrencyAmount operator +(CurrencyAmount a, CurrencyAmount b)
+    {
+        if(a.currency != b.currency)
+        {
+            throw new ArgumentException();
+        }
+
+        return new CurrencyAmount(a.amount + b.amount, a.currency);
+    }
+
+    public static CurrencyAmount operator -(CurrencyAmount a, CurrencyAmount b)
+    {
+        if (a.currency != b.currency)
+        {
+            throw new ArgumentException();
+        }
+
+        return new CurrencyAmount(a.amount - b.amount, a.currency);
+    }
+
+    public static CurrencyAmount operator *(CurrencyAmount a, CurrencyAmount b)
+    {
+        if (a.currency != b.currency)
+        {
+            throw new ArgumentException();
+        }
+
+        return new CurrencyAmount(a.amount * b.amount, a.currency);
+    }
+
+    public static CurrencyAmount operator *(CurrencyAmount a, decimal b)
+    {
+        return new CurrencyAmount(a.amount * b, a.currency);
+    }
+
+    public static CurrencyAmount operator /(CurrencyAmount a, CurrencyAmount b)
+    {
+        if(b.amount == 0)
+        {
+            throw new DivideByZeroException();
+        }
+
+        if (a.currency != b.currency)
+        {
+            throw new ArgumentException();
+        }
+
+        return new CurrencyAmount(a.amount / b.amount, a.currency);
+    }
+
+    public static CurrencyAmount operator /(CurrencyAmount a, decimal b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException();
+        }
+
+        return new CurrencyAmount(a.amount / b, a.currency);
+    }
+
+    // Implement type conversion operators
+    public static explicit operator double(CurrencyAmount ca) => (double)ca.amount;
+    public static implicit operator decimal(CurrencyAmount ca) => ca.amount;
+}
---
+++ C#/Wizards and Warriors 2.0/Program.cs
@@ -0,0 +1,22 @@
+﻿var destination = new Destination
+{
+    Name = "Vo Mimbre",
+    Inhabitants = 332
+};
+
+var characterA = new Character
+{
+    Class = "Warrior",
+    Level = 1,
+    HitPoints = 30
+};
+
+var characterB = new Character
+{
+    Class = "Mage",
+    Level = 20,
+    HitPoints = 2000
+};
+
+Console.WriteLine(GameMaster.Describe(characterA, destination));
+Console.WriteLine(GameMaster.Describe(characterB, destination, TravelMethod.Horseback));
