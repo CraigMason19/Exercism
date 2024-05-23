f92af0701c17aa7638b7031c37bf4c41493bf9ae
SadHappy <craigjm19@gmail.com>
2024-05-23 13:49:52 +0100

    Added Eliuds Eggs solution in C#

---
+++ C#/Eliuds Eggs/EliudsEggs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class EliudsEggs
+{
+    public static string ToBinary(int value)
+    {
+        return Convert.ToString(value, 2);
+    }
+
+    public static int EggCount(int encodedCount)
+    {
+        string binary = ToBinary(encodedCount);
+        return binary.Where(bit => bit == '1').ToArray().Length;
+    }
+}
---
+++ C#/Eliuds Eggs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+var displayValues = new List<int>()
+{
+    0,
+    1,
+    4,
+    16,
+    13,
+    89,
+    2_000_000_000,
+};
+
+foreach (int value in displayValues)
+{
+    Console.WriteLine($"Value: { value}");
+    Console.WriteLine($"\tBinary: { EliudsEggs.ToBinary(value)}");
+    Console.WriteLine($"\tEgg Count: {EliudsEggs.EggCount(value)}");
+}
