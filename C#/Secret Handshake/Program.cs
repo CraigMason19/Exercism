bda48b7bc33c2580c95db20de321f18a6719c35b
SadHappy <craigjm19@gmail.com>
2024-05-23 14:15:37 +0100

    Added Secret Handshake solution in C#

---
+++ C#/Secret Handshake/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var codes = new List<int>()
+{
+    1,
+    9,
+    23,
+    31,
+};
+
+foreach(var code in codes)
+{
+    var actions = SecretHandshake.Commands(code);
+    Console.WriteLine(string.Join(", ", actions));
+}
---
+++ C#/Secret Handshake/SecretHandshake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 00001 = wink
+// 00010 = double blink
+// 00100 = close your eyes
+// 01000 = jump
+// 10000 = Reverse the order of the operations in the secret handshake.
+
+public static class SecretHandshake
+{
+    public static readonly string[] Actions = ["jump", "close your eyes", "double blink", "wink"];
+    public static readonly int CodeLength = 5;
+
+    public static string ToBinary(int value)
+    {
+        return Convert.ToString(value, 2).PadLeft(CodeLength, '0');
+    }
+
+    public static string[] Commands(int commandValue)
+    {
+        var actions = new List<string>();
+        var binary = ToBinary(commandValue);
+
+        for (int i = 1; i < CodeLength; i++)
+        {
+            if (binary[i] == '1')
+            {
+                actions.Add(Actions[i - 1]);
+            }
+        }
+
+        if(binary[0] == '1' == false)
+        {
+            actions.Reverse();
+        }
+
+        return actions.ToArray();
+    }
+}
