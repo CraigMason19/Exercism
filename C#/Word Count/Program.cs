1d7dcc3edffae71cbc553cac47faa8f3ac7d8361
SadHappy <craigjm19@gmail.com>
2024-05-23 21:42:30 +0100

    Added Word Count solution in C#

---
+++ C#/Word Count/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+List<string> inputs = new()
+{
+  "reserved words like constructor and toString ok?",
+  " multiple   whitespaces",
+  "First: don't laugh. Then: don't cry. You're getting it.",
+  "go Go GO Stop stop",
+};
+
+foreach (string input in inputs)
+{
+    var d = WordCount.CountWords(input);
+
+    Console.WriteLine(input);
+
+    foreach (KeyValuePair<string, int> kvp in d)
+    {
+        Console.WriteLine("\tK:{0}, V:{1}", kvp.Key, kvp.Value);
+    }
+};
---
+++ C#/Word Count/WordCount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public static class WordCount
+{
+    public static string RemovePunctuation(string str)
+    {
+        string[] punctuation = [".", "\n", "?", ":", ",", "!", "&", "@", "$", "%", "^", "&"];
+
+        foreach(var c in punctuation)
+        { 
+            str = str.Replace(c, " ");
+        };
+
+        return str;
+    }
+
+    public static string Clean(string str)
+    { 
+        // Remove leading and trailing quotes if present
+        str = Regex.Replace(str, @"^['""]|['""]$", "");
+
+        return str.ToLower().Trim();
+    }
+
+    public static IDictionary<string, int> CountWords(string phrase)
+    {
+        var d = new Dictionary<string, int>();
+
+        var words = RemovePunctuation(phrase).Split(" ")
+                    .Select(x => Clean(x))
+                    .Where(x => x != "") 
+                    .ToArray();
+
+        foreach (var word in words)
+        {
+            if (d.ContainsKey(word))
+            {
+                d[word] += 1;
+            }
+            else
+            {
+                d[word] = 1;
+            }
+        }
+
+        return d;
+    }
+}
