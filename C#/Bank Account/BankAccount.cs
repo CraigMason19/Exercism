8076e68c0a22eeffebb0fe5e4eec854d62645684
SadHappy <craigjm19@gmail.com>
2024-05-24 16:04:44 +0100

    Added Bank Account solution in C#

---
+++ C#/Bank Account/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class BankAccount
+{
+    private decimal _balance;
+    private bool _isOpen;
+
+    public BankAccount()
+    {
+        this._balance = 0;
+        this._isOpen = false;
+    }
+
+    public void Open()
+    {
+        this._balance = 0;
+        this._isOpen = true;
+    }
+
+    public void Close()
+    {
+        this._balance = 0;
+        this._isOpen = false;
+    }
+
+    public decimal Balance
+    {
+        get
+        {
+            if(this._isOpen == false)
+            {
+                throw new System.InvalidOperationException();
+            }
+
+            return this._balance;
+        }
+    }
+
+    public void UpdateBalance(decimal change)
+    {
+        if (this._isOpen == false)
+        {
+            return;
+        }
+
+        this._balance += change;
+    }
+}
---
+++ C#/Bank Account/Program.cs
@@ -0,0 +1,16 @@
+﻿var account = new BankAccount();
+account.Open();
+account.UpdateBalance(50);
+
+Console.WriteLine(account.Balance);
+
+account.Close();
+
+try
+{
+    Console.WriteLine(account.Balance);
+}
+catch (Exception e)
+{
+	Console.WriteLine(e.Message);
+}
