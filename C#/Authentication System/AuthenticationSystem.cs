000f4244f9a4dc1e639f5866bb197e638c98bd76
SadHappy <craigjm19@gmail.com>
2024-07-09 19:54:05 +0100

    Added Authentication System in C#

---
+++ C#/Authentication System/AuthenticationSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+public class Authenticator
+{
+    private class EyeColor
+    {
+        public const string Blue = "blue";
+        public const string Green = "green";
+        public const string Brown = "brown";
+        public const string Hazel = "hazel";
+        public const string Grey = "grey";
+    }
+
+    private Identity admin;
+
+    public Identity Admin
+    {
+        get { return admin; }
+        set { admin = value; }
+    }
+
+    private readonly IDictionary<string, Identity> developers;
+
+    public Authenticator(Identity admin)
+    {
+        this.admin = admin;
+
+        // Initialize the mutable dictionary
+        var mutableDevelopers = new Dictionary<string, Identity>
+        {
+            ["Bertrand"] = new Identity
+            {
+                Email = "bert@ex.ism",
+                EyeColor = EyeColor.Blue
+            },
+            ["Anders"] = new Identity
+            {
+                Email = "anders@ex.ism",
+                EyeColor = EyeColor.Brown
+            }
+        };
+
+        developers = new ReadOnlyDictionary<string, Identity>(mutableDevelopers);
+    }
+
+    public IDictionary<string, Identity> GetDevelopers() => developers;
+}
+
+public struct Identity
+{
+    public string Email { get; set; }
+
+    public string EyeColor { get; set; }
+}
---
+++ C#/Authentication System/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+var admin = new Identity
+{
+    EyeColor = "green",
+    Email = "admin@ex.ism"
+};
+
+var authenticator = new Authenticator(admin);
+
+Console.WriteLine($"EyeColor: {authenticator.Admin.EyeColor}\nEmail: {authenticator.Admin.Email}");
