6cdab3dfeb188bb6cf52a1a27161015211e636af
SadHappy <craigjm19@gmail.com>
2024-07-13 12:32:03 +0100

    Added Armstrong Numbers solution in C#

---
+++ C#/Armstrong Numbers/ArmstrongNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        var numbers = number.ToString().ToCharArray().Select(x => Convert.ToInt32(x.ToString()));
+        var result = numbers.Aggregate(0, (acc, x) => acc + (int)Math.Pow(x, numbers.Count()));
+
+        return number == result;
+    }
+}
---
+++ C#/Armstrong Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿var numbers = new[] {
+    9,
+    10,
+    153,
+    154,
+    9926314,
+};
+
+foreach(var n in numbers)
+{
+    Console.WriteLine($"{n} -> {ArmstrongNumbers.IsArmstrongNumber(n)}");
+}
