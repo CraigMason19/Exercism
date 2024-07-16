dc7790a1b8c069e82ee1b3d88406be227957a9df
SadHappy <craigjm19@gmail.com>
2024-07-16 09:06:12 +0100

    Added Raindrops solution in C#

---
+++ C#/Raindrops/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+var numbers = new int[] {28, 30, 34};
+
+foreach(var n in numbers)
+{
+    Console.WriteLine($"{n} -> {Raindrops.Convert(n)}");
+}
---
+++ C#/Raindrops/Raindrops.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        var pling = number % 3 == 0 ? "Pling" : "";
+        var plang = number % 5 == 0 ? "Plang" : "";
+        var plong = number % 7 == 0 ? "Plong" : "";
+
+        var result = pling + plang + plong;
+
+        return string.IsNullOrEmpty(result) ? number.ToString() : $"{result}";     
+    }
+}
