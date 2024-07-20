8bd2d85860dd93ea287ad6e5c6f3b1123f1caf6c
SadHappy <craigjm19@gmail.com>
2024-07-20 11:20:09 +0100

    Added Building Telemetry solution in C#

---
+++ C#/Building Telemetry/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+var car = RemoteControlCar.Buy();
+car.SetSponsors("Exercism", "Walker Industries", "Acme Co.");
+
+Console.WriteLine(car.DisplaySponsor(sponsorNum: 1)); // => "Walker Industries"
+
+car.Drive();
+car.Drive();
+var tc = new TelemetryClient(car);
+int serialNum = 4;
+Console.WriteLine(tc.GetBatteryUsagePerMeter(serialNum)); // => "usage-per-meter=5"
+
+serialNum = 1;
+Console.WriteLine(tc.GetBatteryUsagePerMeter(serialNum)); // => "no data"
